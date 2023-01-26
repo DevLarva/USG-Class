ab57d74d5f2b47dd6ab095ad24698c567d1ac6ed
백대홍 <bdh3620@naver.com>
2023-01-26 14:57:37 +0900

    NewDiscoverView image add, scroll view

--- USG01/TabGroup/NewDiscoverView.swift
+++ USG01/TabGroup/NewDiscoverView.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct NewDiscoverView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                Image("new main")
+                    .resizable()
+                    .frame(width: 400, height: 300)
+                    .padding()
+                RoundedRectangle(cornerRadius: 25, style: .continuous)
+                    .foregroundColor(.systemGray4)
+                    .overlay(VStack(spacing: 15) {
+                        Text("Explore the Human Interface Guidelines")
+                            .font(.title2)
+                            .bold()
+                        Text("Learn best practices that can help you design a great experience for any Apple platform")
+                            
+                    }.padding(.leading))
+                    .frame(width: 350, height: 100)
+                    .padding()
+                
+            }
+            .navigationTitle("새로운 발견")
+        }
     }
 }
-
+extension Color {
+    static let systemGray4 = Color("systemGray4")
+}
 struct NewDiscoverView_Previews: PreviewProvider {
     static var previews: some View {
         NewDiscoverView()
