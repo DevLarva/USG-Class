93747436d17e1475042626e8cd69c7f42bb1f4d2
백대홍 <bdh3620@naver.com>
2023-01-28 23:42:05 +0900

    AroundView edit

--- USG01/TabGroup/AroundView.swift
+++ USG01/TabGroup/AroundView.swift
@@ -9,34 +9,46 @@ import SwiftUI
 
 struct AroundView: View {
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 10) {
-                HStack {
-                    Image(systemName: "newspaper")
-                    Text("뉴스")
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 12) {
+                    HStack {
+                        Image(systemName: "newspaper")
+                        Text("뉴스")
+                    }
                     
-                }
-                Divider()
-                HStack {
-                    Image(systemName: "bookmark")
-                    Text("책갈피")
-                }
-                Divider()
-                HStack {
-                    Image(systemName: "arrow.down.circle")
-                    Text("다운로드됨")
+                    Divider()
+                    
+                    HStack {
+                        Image(systemName: "bookmark")
+                        Text("책갈피")
+                    }
+                    Divider()
+                    
+                    HStack {
+                        Image(systemName: "arrow.down.circle")
+                        Text("다운로드됨")
+                        
+                    }
+                    Spacer(minLength: 10)
+                    
+                    HStack {
+                        Text("이어서 보기").bold()
+                            .font(.title2)
+                    }
+                    
+                    HStack {
+                        Text("주제").bold()
+                            .font(.title2)
+                    }
                 }
                 
+                .padding()
                 
                 
                 
             }
-            .padding()
-            
-            VStack(alignment: .leading, spacing: 10) {
-                Text("이어서 보기")
-            }
-            
+            .navigationTitle("둘러보기")
         }
     }
 }
