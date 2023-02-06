7c1e1a721d000875acdb8f3b81a3d266580e1ca9
백대홍 <bdh3620@naver.com>
2023-02-06 21:40:40 +0900

    USG05 start and movielist add

---
+++ USG04/USG04/MovieListView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieListView.swift
+//  USG04
+//
+//  Created by 백대홍 on 2023/02/06.
+//
+
+import SwiftUI
+
+struct MovieListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieListView()
+    }
+}
